a9a374f0db7731b21ada710bc831bacc72c56d33
John Yevsukov <github.com@john.engineering>
2021-06-11 17:56:38 -0400

    added fourth table. overlooked it before

--- data/migrations/20210611164542_first-migration.js
+++ data/migrations/20210611164542_first-migration.js
@@ -25,10 +25,28 @@ exports.up = function(knex) {
             .onDelete('CASCADE')
             .onUpdate('CASCADE')
     })
+    .createTable('resource_assignment', table => {
+      table.increments('resource_assignment_id')
+      table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+      table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('resource_assignment')
     .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects')
